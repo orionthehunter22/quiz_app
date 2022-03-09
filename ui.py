27d685170b5d3d24f3316de918cd45e510470ca9
orionthehunter22 <orionthehunter22@gmail.com>
2022-03-09 00:33:45 -0600

    fixed the question text

--- ui.py
+++ ui.py
@@ -1,16 +1,25 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(pady=20, padx=20, bg=THEME_COLOR)
         self.canvas = Canvas(width=300, height=250, bg="white")
-        self.question_text = self.canvas.create_text(150, 125, text="Test Test Double Test", font=("Arial", 20, "italic"), fill=THEME_COLOR)
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            width= 280,
+            text="Test Test Double Test",
+            font=("Arial", 20, "italic"),
+            fill=THEME_COLOR
+        )
         self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
 
         self.score_label = Label(text="Score:0", fg="white", bg=THEME_COLOR)
@@ -24,5 +33,10 @@ class QuizInterface:
         self.false_button = Button(image=false_img, highlightthickness=0)
         self.false_button.grid(column=1, row=2)
 
+        self.get_next_question()
 
         self.window.mainloop()
+
+    def get_next_question(self):
+        question_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=question_text)
