03b71b3fecc486d2e7af1acc82b11adec9a6d9cd
orionthehunter22 <orionthehunter22@gmail.com>
2022-03-11 00:39:08 -0600

    Finished the Quiz game

--- ui.py
+++ ui.py
@@ -15,7 +15,7 @@ class QuizInterface:
         self.question_text = self.canvas.create_text(
             150,
             125,
-            width= 280,
+            width=280,
             text="Test Test Double Test",
             font=("Arial", 20, "italic"),
             fill=THEME_COLOR
@@ -26,11 +26,11 @@ class QuizInterface:
         self.score_label.grid(column=1, row=0)
 
         true_img = PhotoImage(file="./images/true.png")
-        self.true_button = Button(image=true_img, highlightthickness=0)
+        self.true_button = Button(image=true_img, highlightthickness=0, command=self.true_pressed)
         self.true_button.grid(column=0, row=2)
 
         false_img = PhotoImage(file="./images/false.png")
-        self.false_button = Button(image=false_img, highlightthickness=0)
+        self.false_button = Button(image=false_img, highlightthickness=0, command=self.false_pressed)
         self.false_button.grid(column=1, row=2)
 
         self.get_next_question()
@@ -38,5 +38,26 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
-        question_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=question_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            question_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=question_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+
+    def true_pressed(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+
+    def false_pressed(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        elif is_right:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
